8a4494801a43162859b1080e12737ec579d1b72b
sanya96 <sanya042014@gmail.com>
2020-01-30 21:27:12 +0200

    add footer and other sections

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-(function () {
-        this.goTop = $('#go-top');
-        $(document).on('scroll', () =>{
-            ($(this).scrollTop() >= 250) ? this.goTop.fadeIn() : this.goTop.fadeOut();
-        });
-
-        this.goTop.on('click', () =>{
-            $('html').animate({scrollTop:0},1000);
-        })
-})();
+// (function () {
+//         this.goTop = $('#go-top');
+//         $(document).on('scroll', () =>{
+//             ($(this).scrollTop() >= 250) ? this.goTop.fadeIn() : this.goTop.fadeOut();
+//         });
+//
+//         this.goTop.on('click', () =>{
+//             $('html').animate({scrollTop:0},1000);
+//         })
+// })();
 
 $(function () {
     $(window).scroll(function(event) {
@@ -35,4 +35,3 @@ $(function () {
 //         });
 //     });
 // });
-
